84fc0694159e349331aead72ed56e1213c4685a6
Kniffte <sebschreiber@gmx.net>
2022-02-22 21:54:59 +0100

    binary solution finished

--- intermedidiate-algo/binary.js
+++ intermedidiate-algo/binary.js
@@ -1,5 +1,11 @@
 function binaryAgent(str) {
-    return str;
+    const words = str.split(" ");
+    const sentence = [];
+    for (let i = 0; i < words.length; i++) {
+        let converted = parseInt(words[i], 2);
+        sentence.push(String.fromCharCode(converted));
+    }
+    return sentence.join("");
 }
   
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
