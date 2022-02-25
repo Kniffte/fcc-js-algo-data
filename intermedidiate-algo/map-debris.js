1cfd51561cb2c58c64ca0caf1ea9b59d60db56ea
Kniffte <sebschreiber@gmx.net>
2022-02-25 23:29:50 +0100

    map debris solution added

---
+++ intermedidiate-algo/map-debris.js
@@ -0,0 +1,11 @@
+function orbitalPeriod(arr) {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    for (let obj of arr) {
+        obj.orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt((earthRadius + obj.avgAlt)**3 / GM));
+        delete obj.avgAlt;
+    }
+    return arr;
+}
+  
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
