3e2afcb049eefcfe6cd8747addd83add9c95bed3
Kniffte <sebschreiber@gmx.net>
2022-02-28 19:57:04 +0100

    tel no validator solution added

--- projects/tel-no-validator.js
+++ projects/tel-no-validator.js
@@ -1,6 +1,10 @@
 function telephoneCheck(str) {
-    let no = str.match(/^1?/gi);
-    return true;
+    if (str.match(/^1?[- ]?([(][0-9]{3}[)]|[0-9]{3})[- ]?[0-9]{3}[- ]?[0-9]{4}$/gi)) {
+        return true
+    };
+    return false;
 }
   
 telephoneCheck("555-555-5555");
+// telephoneCheck("555-5555");
+// telephoneCheck("27576227382");
